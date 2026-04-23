73bb223653b655dc01fe166948bdbb8f785dc62b
Yoann <Yoann@MacBook-de-Yoann.local>
2026-04-23 16:36:25 +0200

    Fix Supabase key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://piaeboemsrpfxepissbt.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpYWVib2Vtc3JwZnhlcGlzc2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDYxOTgsImV4cCI6MjA5MjUyMjE5OH0.Nv-CxKaWgwIfCK4c-DOSgEVdi_js6YkdRaJ2cx0j0J4'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
